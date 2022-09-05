7a04679310ab7ff7270b29cb2d03dfb15b62998b
sunzehui <501574022@qq.com>
2022-09-06 07:50:40 +0800

    fix: set chart fixed height

--- src/components/weather-line-chart.tsx
+++ src/components/weather-line-chart.tsx
@@ -1,85 +1,86 @@
 import React from "react";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js";
-import {Line} from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import dayjs from "dayjs";
-import {useRealTimeWeather, useWeather} from "../hooks/useWeather";
+import { useRealTimeWeather } from "@/hooks/useWeather";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    responsive: true,
-    tension: 0.5,
-    scales: {
-        yAxes:
-            {
-                ticks: {
-                    // y轴包含一个符号
-                    callback: function (value, index, values) {
-                        return value + '℃';
-                    }
-                }
-            }
-
-    },
-    plugins: {
-        legend: {
-            position: "top" as const,
+  maintainAspectRatio: false,
+  tension: 0.5,
+  scales: {
+    yAxes: {
+      ticks: {
+        // y轴包含一个符号
+        callback: function (value, index, values) {
+          return value + "℃";
         },
-        title: {
-            display: true,
-            text: "temp Line Chart",
-        },
+      },
     },
+  },
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "temp Line Chart",
+    },
+  },
 };
 
-
 export function WeatherLineChart(props) {
-    const {data: realTimeData, isLoading, error} = useRealTimeWeather({lat: props.lat, lon: props.lon});
-    if (isLoading) return <>loading...</>;
-    if (error) return <>{error.message}</>;
-    const forecast = realTimeData.daily;
-    const forecastIdx = 0;
-    const data = {
-        labels: ["Morning", "Day", "Evening", "Night"],
-        datasets: [
-            {
-                label: "温度",
-                data: [
-                    forecast[forecastIdx].temp.morn,
-                    forecast[forecastIdx].temp.day,
-                    forecast[forecastIdx].temp.eve,
-                    forecast[forecastIdx].temp.night,
-                ],
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-                pointBorderWidth: 0.2,
-            },
+  const {
+    data: realTimeData,
+    isLoading,
+    error,
+  } = useRealTimeWeather({ lat: props.lat, lon: props.lon });
+  if (isLoading) return <>loading...</>;
+  if (error) return <>{error.message}</>;
+  const forecast = realTimeData.daily;
+  const forecastIdx = 0;
+  const data = {
+    labels: ["Morning", "Day", "Evening", "Night"],
+    datasets: [
+      {
+        label: "温度",
+        data: [
+          forecast[forecastIdx].temp.morn,
+          forecast[forecastIdx].temp.day,
+          forecast[forecastIdx].temp.eve,
+          forecast[forecastIdx].temp.night,
         ],
-    };
-    return (
-        <div className="card w-full h-full  glass">
-            <div className="card-body  w-full relative">
-                <Line options={options} data={data} height={200} width={1000}/>
-            </div>
-        </div>
-    );
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        pointBorderWidth: 0.2,
+      },
+    ],
+  };
+  return (
+    <div className="card w-full h-full  glass">
+      <div className="card-body h-[250px] w-full relative">
+        <Line options={options} data={data} width={100} height={100} />
+      </div>
+    </div>
+  );
 }
